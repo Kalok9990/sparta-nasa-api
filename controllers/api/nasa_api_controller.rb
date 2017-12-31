771059e537e84b160e46813e6eb5b58dddfebf2d
Tech-A29 <Tech-A29@Tech-A29s-Air.lan>
2017-12-31 14:17:56 +0000

    delete button in stars added

--- controllers/api/nasa_api_controller.rb
+++ controllers/api/nasa_api_controller.rb
@@ -2,48 +2,76 @@ require 'neows'
 
 class NasaApiController < Sinatra::Base
 
+  # Sets root as the parent-directory of the current file
   set :root, File.join(File.dirname(__FILE__), "../..")
+
+  # Sets the view directory correctly
   set :views, Proc.new{File.join(root, "views")}
+
+  # Sets a new client for the gem neows
   client = Neows::REST::Client.new
-  @key = "api_key=92KjKXlfAG8eYrh0KhkkRuWKAbIwdEUAsu2wlS5c"
+
+  # Enables the reloader so we dont need to keep restarting the server
   configure :development do
     register Sinatra::Reloader
   end
 
+  # A get request to the api browse service which will respond with the data that is fetched
   get '/api/browse' do
 
+    # A gem method that uses the browse service
     api_result = client.browse
     output = ''
 
+    # Loops through the hash and fetches the relevant information and adds a column to output
     api_result['near_earth_objects'].each do |w|
       neo_ref_id = w['neo_reference_id']
       name = w['name']
       output << "<tr><td><a href='/api/<%= @neo_ref_id %><%= #{neo_ref_id} %>'>#{neo_ref_id}</td></a><td>#{name}</td></tr>"
     end
 
+    # renders on browse page
     erb :'nasa/browse', :locals => {results: output}
   end
 
+  # renders a lookup page
   get '/api/lookup' do
     erb :'nasa/lookup'
   end
 
+  # A post request that receives a user input and fetches information accordingly
   post '/api/' do
     id = params[:id]
     id_url = "https://api.nasa.gov/neo/rest/v1/neo/#{id}?api_key=92KjKXlfAG8eYrh0KhkkRuWKAbIwdEUAsu2wlS5c"
     response = HTTParty.get(id_url)
     @feed = response.parsed_response
+    @neo_id = @feed['neo_reference_id']
+    @name = @feed['name']
+    @km_max = @feed['estimated_diameter']['kilometers']['estimated_diameter_max']
+    @km_min = @feed['estimated_diameter']['kilometers']['estimated_diameter_min']
+    @m_max = @feed['estimated_diameter']['meters']['estimated_diameter_max']
+    @m_min = @feed['estimated_diameter']['meters']['estimated_diameter_min']
+    @miles_max = @feed['estimated_diameter']['miles']['estimated_diameter_max']
+    @miles_min = @feed['estimated_diameter']['miles']['estimated_diameter_min']
+    @feet_max = @feed['estimated_diameter']['feet']['estimated_diameter_max']
+    @feet_min = @feed['estimated_diameter']['feet']['estimated_diameter_min']
+    @hazardous = @feed['is_potentially_hazardous_asteroid']
+    @link = @feed['links']['self']
+
+    # renders on show page
     erb :'nasa/show'
   end
 
+  # renders a feed page
   get '/api/feed' do
     erb :'nasa/feed'
   end
 
+  # A post request that receives a user input and fetches information accordingly
   post '/api/dates/' do
-    startdate = params[:start_date]
-    enddate = params[:end_date]
-    if startdate == "" && enddate == ""
+    @startdate = params[:start_date]
+    @enddate = params[:end_date]
+    if @startdate == "" && @enddate == ""
       id_url = "https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key=92KjKXlfAG8eYrh0KhkkRuWKAbIwdEUAsu2wlS5c"
     else
       id_url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=#{startdate}&end_date=#{enddate}&api_key=92KjKXlfAG8eYrh0KhkkRuWKAbIwdEUAsu2wlS5c"
@@ -56,6 +84,7 @@ class NasaApiController < Sinatra::Base
     @neow = @feed['near_earth_objects']
     @date = @feed['near_earth_objects'].keys
 
+    # renders on dates page
     erb :'nasa/dates'
   end
 end
--- controllers/stars_controller.rb
+++ controllers/stars_controller.rb
@@ -2,18 +2,23 @@ class StarsController < Sinatra::Base
 
   # sets root as the parent directory of the current file
   set :root, File.join(File.dirname(__FILE__), "..")
+
+  # Sets the view directory correctly
   set :views, Proc.new{File.join(root, "views")}
 
+  # Enables the reloader so we dont need to keep restarting the server
   configure :development do
     register Sinatra::Reloader
   end
 
+  # A get request to the / route will respond with our index template with all the stars from the model
   get "/" do
-    @title = "Stars"
+    @title = "Constellations"
     @stars = Star.all
     erb :"stars/index"
   end
 
+  # A post request to / will create a new post with the imformation the user entered which is stored in the params
   post "/" do
     star = Star.new
     star.title = params[:title]
@@ -23,18 +28,21 @@ class StarsController < Sinatra::Base
     redirect "/"
   end
 
+  # A get request to /new will respond with a template with our new form that the user can complete to add a new star
   get "/new" do
     @title = "New Star Form"
     @star = Star.new
     erb :"stars/new"
   end
 
+  # A get request to /:id will respond with a the show template with the requested post
   get "/:id" do
     id = params[:id].to_i
     @star = Star.find(id)
     erb :"stars/show"
   end
 
+  # A request to /:id/edit will respond with a the edit template with the star data of the star we can want to update
   get "/:id/edit" do
     id = params[:id].to_i
     @star = Star.find(id)
@@ -42,6 +50,7 @@ class StarsController < Sinatra::Base
     erb :"stars/edit"
   end
 
+  # A put request to the /:id will will update an existing star
   put "/:id" do
     id = params[:id]
     star = Star.find(id)
@@ -52,6 +61,7 @@ class StarsController < Sinatra::Base
     redirect '/'
   end
 
+  # A delete request to /:id will delete the specified post from the db
   delete "/:id" do
     id = params[:id]
     Star.destroy(id)
